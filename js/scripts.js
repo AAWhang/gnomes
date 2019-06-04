2efa78533eae8aa48f05babe624a362f5c7e8d03
Aaron Whang and Jessica Munoz <jessica@>
2019-06-04 10:17:16 -0700

    It will have intial files and jsquery function of alterinf page.

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function()
+  {
+    $(".gnomeToggle").click(function()
+      {
+        $("#gnomeShow").toggle();
+        $("#gnomeHide").toggle();
+      });
+    $(".clickable").click(function() {
+        $("#hiddenText").toggle();
+        $("#showingText").toggle();
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
